f7ea0bff0e8fc6b64f7ee45a12b1458ef25d7928
vinnarasan <vasanthan93c@gmail.com>
2018-02-10 23:13:24 +0530

    pass removed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,7 +76,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 DEFAULT_FROM_EMAIL = "vinnarasan.c@gmail.com"
 EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
-EMAIL_HOST_PASSWORD = 'g00Gl3@tech'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
